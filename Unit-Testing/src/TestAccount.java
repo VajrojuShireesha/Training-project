a98ed6e0ba5f4488b5cd8e8c77c88f42ec8afd5c
VajrojuShireesha <Vajroju.Shireesha@ibm.com>
2022-07-19 22:51:20 +0530

    unit-testing

---
+++ Unit-Testing/src/TestAccount.java
@@ -0,0 +1,28 @@
+import static org.junit.jupiter.api.Assertions.assertEquals;
+import static org.junit.jupiter.api.Assertions.assertThrows;
+
+import org.junit.jupiter.api.BeforeEach;
+import org.junit.jupiter.api.Test;
+
+
+
+public class TestAccount {
+	private Account acnt;
+	
+	@BeforeEach
+	public void init() {
+		acnt = new Account(1000);
+	}
+	
+	@Test
+	public void testValidDeposit() {
+		assertEquals(2000, acnt.deposit(1000));
+	}
+	
+	@Test
+	public void testNegativeDeposit() {
+		assertThrows(NumberFormatException.class, () -> acnt.deposit(-100));
+		
+	}
+	
+}
